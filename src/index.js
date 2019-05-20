ea35492891ea901c40ad758a62c99030e51bd6e3
yaya <1286249672@qq.com>
2019-05-20 10:11:17 +0800

    [dev]增加htmlplugin

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import _ from 'lodash';
 import './style.css';
+import printMe from './print.js';
+import '../src/component/index/home.css'
+
 
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const element = document.createElement('div');
+  var btn = document.createElement('button');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+
+  // Lodash, currently included via a script, is required for this line to work
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('btn');
+  return element;
+}
+
+document.body.appendChild(component());
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,76 @@
 const path = require('path');
+//解决新建js文件并打包后dist/index.html 引入名称未更新
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+
 //把css文件抽离一个单独的style.css文件而不是直接插入到head中
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+// 创建多个实例
+const extractCSS = new ExtractTextPlugin('stylesheets/[name]-one.css');
+
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 
 
 //path.resolve把一个路径或路径片段的序列解析为一个绝对路径
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   output: {
-    filename: 'bundle.js',
+    // filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
+        use: extractCSS.extract({
           fallback: "style-loader",
-          use: "css-loader"
+          use: [
+            { loader: 'css-loader', options: { importLoaders: 1 } },
+            // {
+            //   loader: 'postcss-loader',
+            //   options: {
+            //     ident: 'postcss',
+            //     plugins: (loader) => [
+            //       require('postcss-import')({ root: loader.resourcePath }),
+            //       require('postcss-cssnext')(),
+            //       require('autoprefixer')(),
+            //       require('cssnano')()
+            //     ]
+            //   }
+            // }
+
+          ]
         })
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin("styles.css"),
+    new CleanWebpackPlugin(),
+    new ExtractTextPlugin("style/[name].css"),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      filename: 'index.html',
+      inject: false,
+      template: require('html-webpack-template'),
+    }),
+    new ManifestPlugin({
+      fileName: 'my-manifest.json',
+      basePath: '/app/',
+      seed: {
+        name: 'My Manifest'
+      }
+    }),
+
   ]
 };
