4d2bb81e398f914e9554ee52e0109dfb4dc95cb8
yaya <1286249672@qq.com>
2019-05-23 00:23:01 +0800

    [dev]添加build env设置生产环境设置sideefectcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,8 @@
- 
-module.exports = (ctx) => ({
-    parser: ctx.parser ? 'sugarss' : false,
-    map: ctx.env === 'development' ? ctx.map : false,
-    plugins: {
-      cssnano: ctx.env === 'production' ? {} : false,
-      
-    }
-  })
+ module.exports = (ctx) => ({
+     parser: ctx.parser ? 'sugarss' : false,
+     map: ctx.env === 'development' ? ctx.map : false,
+     plugins: {
+         cssnano: ctx.env === 'production' ? {} : false,
+
+     }
+ })
--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const dev = process.env.NODE_ENV !== 'production';
-
+console.log('dev=============================》', dev)
 
 module.exports = {
     entry: {
@@ -31,41 +31,41 @@ module.exports = {
         rules: [{
             test: /\.(sa|sc|c)ss$/,
             use: [{
-                loader: dev ? 'style-loader' : MiniCssExtractPlugin.loader,
-                options: {
-                    hmr: process.env.NODE_ENV === 'development',
+                    loader: dev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    options: {
+                        hmr: process.env.NODE_ENV === 'development',
+                    },
                 },
-            },
-            {
-                loader: 'css-loader',
-                options: {
-                    importLoaders: 1,
-                }
-            },
-            {
-                loader: 'postcss-loader',
-                options: {
-                    plugins: [
-                        require('precss')(), //sass解析
-                        require('cssnano')(), //相同css合并（例：.a{width:100px},.b{width:100px} => .a,.b{width:100px}）
-                        require('autoprefixer')({
-                            browsers: [
-                                '>0%',
-                                'last 4 versions',
-                                'Firefox ESR',
-                                'not ie < 8'//针对不同浏览器某些css属性需要用到不同内核前缀（例：-webkit-transfrom，-moz-transfrom)
-                            ]
-                        })
-                    ],
-                    sourceMap: true,
-                    config: {
-                        path: 'postcss.config.js'
+                {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 1,
                     }
                 },
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: [
+                            require('precss')(), //sass解析
+                            require('cssnano')(), //相同css合并（例：.a{width:100px},.b{width:100px} => .a,.b{width:100px}）
+                            require('autoprefixer')({
+                                browsers: [
+                                    '>0%',
+                                    'last 4 versions',
+                                    'Firefox ESR',
+                                    'not ie < 8' //针对不同浏览器某些css属性需要用到不同内核前缀（例：-webkit-transfrom，-moz-transfrom)
+                                ]
+                            })
+                        ],
+                        sourceMap: true,
+                        config: {
+                            path: 'postcss.config.js'
+                        }
+                    },
 
-            },
+                },
             ],
-        },]
+        }, ]
     },
     optimization: {
         splitChunks: {
