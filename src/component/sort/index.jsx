e7b08d550965da5d72d1eeb9241466fe79785687
yaya <1286249672@qq.com>
2019-05-31 10:06:24 +0800

    dev: 测试commit

--- src/component/sort/index.jsx
+++ src/component/sort/index.jsx
@@ -5,7 +5,7 @@ import React, {Component} from 'react';
 export default class Sortcomponent extends Component{
     render(){
         return(<div>
-            我是分类   分类啊123
+            我是分类   分类啊1234
             我是分类   分类啊
             我是分类   分类啊
             </div>)
