e7332c020621dc5f7dceec9c8d1682368e283638
yaya <1286249672@qq.com>
2019-06-10 10:07:27 +0800

    fix: index.js格式化

--- src/index.js
+++ src/index.js
@@ -6,91 +6,83 @@ const history = require("history").createBrowserHistory();
 
 //rem适配
 // import './plugin/flexible/index.js'
-import 'plugin/flexible/index.js'
+import "plugin/flexible/index.js";
 
 import Indexcomponent from "./component/index/index.jsx";
 import Sortcomponent from "./component/sort/index.jsx";
 
 const rootRoute = {
-    childRoutes: [{
-            path: "/",
-            //   indexRoute: {
-            //     getComponent(nextState, cb) {
-            //       require.ensure(
-            //         [],
-            //         require => {
-            //           cb(null, require("./router/sort")).default;
-            //         },
-            //         "HomePage"
-            //       );
-            //     }
-            //   },
-            component: Application,
-            indexRoute: {
-                /*首页*/
-                onEnter: (nextState, replaceState) => replaceState("/homeIndex")
-            },
-            childRoutes: [{
-                path: "homeIndex",
-                getComponent: require("./router/home.jsx").default,
-                onEnter: () => {
-                    document.title = "首页";
-                }
-            }]
-        },
-        /*未匹配的重定向*/
-        { path: "*", onEnter: (nextState, replaceState) => replaceState("/") }
-    ]
+  childRoutes: [
+    {
+      path: "/",
+      //   indexRoute: {
+      //     getComponent(nextState, cb) {
+      //       require.ensure(
+      //         [],
+      //         require => {
+      //           cb(null, require("./router/sort")).default;
+      //         },
+      //         "HomePage"
+      //       );
+      //     }
+      //   },
+      component: Application,
+      indexRoute: {
+        /*首页*/
+        onEnter: (nextState, replaceState) => replaceState("/homeIndex")
+      },
+      childRoutes: [
+        {
+          path: "homeIndex",
+          getComponent: require("./router/home.jsx").default,
+          onEnter: () => {
+            document.title = "首页";
+          }
+        }
+      ]
+    },
+    /*未匹配的重定向*/
+    { path: "*", onEnter: (nextState, replaceState) => replaceState("/") }
+  ]
 };
 
 console.log(Indexcomponent);
 console.log(Sortcomponent);
 
 function* gen() {
-    yield 1;
+  yield 1;
 }
 var g = gen();
-console.log('gen.next()========>', g.next())
+console.log("gen.next()========>", g.next());
 
 class Application extends Component {
-    render() {
-        return ( <
-            section id = "wap-main" >
-            <
-            Indexcomponent / >
-            <
-            /section>
-        );
-    }
+  render() {
+    return (
+      <section id="wap-main">
+        <Indexcomponent />
+      </section>
+    );
+  }
 }
-render( <
-    Router history = { history } >
-    <
-    ul >
-    <
-    li > < Link to = "/home" > 首页 < /Link></li >
-    <
-    li > < Link to = "/sort" > 关于 < /Link></li >
-    <
-    /ul>
-
-    <
-    hr / >
-    <
-    Switch >
-    <
-    Route path = "/home"
-    component = { Application }
-    /> <
-    Route path = "/sort"
-    component = { Sortcomponent }
-    /> <
-    /Switch> {
-        /* <Switch>
+render(
+  <Router history={history}>
+    <ul>
+      <li>
+        <Link to="/home"> 首页 </Link>
+      </li>
+      <li>
+        <Link to="/sort"> 关于 </Link>
+      </li>
+    </ul>
+    <hr />
+    <Switch>
+      <Route path="/home" component={Application} />
+      <Route path="/sort" component={Sortcomponent} />
+    </Switch>
+    {/* <Switch>
               <Route path="/" exact component={Application} />
               <Route path="/sort" component={Sortcomponent} />
-            </Switch> */
-    } <
-    /Router>,
-    document.getElementById("app")
+            </Switch> */}
+  </Router>,
+  document.getElementById("app")
 );
